01c07aa67c552b80ce00fa42c58a0842d8e5d34d
seven <7b37b3@gmail.com>
2026-04-19 13:05:56 -0300

    refactor: extract games modal to reusable component

--- src/embedded_templates.zig
+++ src/embedded_templates.zig
@@ -6,12 +6,13 @@ pub const EmbeddedTemplates = struct {
 
     auth_login: []const u8 = @embedFile("features/auth/views/login.html"),
     games_index: []const u8 = @embedFile("features/games/views/index.html"),
+    games_modal_game: []const u8 = @embedFile("features/games/views/modal_game.html"),
     home_index: []const u8 = @embedFile("features/home/views/index.html"),
     todo_index: []const u8 = @embedFile("features/todo/views/index.html"),
-    partials_topbar: []const u8 = @embedFile("shared/templates/partials/topbar.html"),
-    partials_sidebar: []const u8 = @embedFile("shared/templates/partials/sidebar.html"),
     partials_bottom_nav: []const u8 = @embedFile("shared/templates/partials/bottom_nav.html"),
     partials_drawer: []const u8 = @embedFile("shared/templates/partials/drawer.html"),
+    partials_sidebar: []const u8 = @embedFile("shared/templates/partials/sidebar.html"),
+    partials_topbar: []const u8 = @embedFile("shared/templates/partials/topbar.html"),
     layout: []const u8 = @embedFile("shared/templates/layout.html"),
 
 };
